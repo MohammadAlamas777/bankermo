65178181c3681a26b2e987e4938e1afdd2114dbc
MohammadAlamas777 <mohammad4atieh@gmail.com>
2026-07-09 23:21:27 +0300

    feat: add TransactionRepository for ledger

---
+++ src/main/java/com/bankermo/bankermo/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.bankermo.bankermo.repository;
+
+import com.bankermo.bankermo.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByAccountIdOrderByCreatedAtDesc(Long accountId);
+}
